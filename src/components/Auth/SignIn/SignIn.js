d5b24f7ef5e0c5c67c3018998b5aceb10091264e
kimssangman <136281131+kimssangman@users.noreply.github.com>
2024-04-30 16:26:56 +0900

    로그인 폼 완료

--- src/components/Auth/SignIn/SignIn.js
+++ src/components/Auth/SignIn/SignIn.js
@@ -3,7 +3,36 @@ import "./SignIn.scss";
 import useSimpleSignIn from "../../../api/signIn/simple_signIn_api";
 
 function SignIn() {
+    // 간편 로그인 훅 분리
     useSimpleSignIn();
+
+    const [form, setForm] = useState({
+        id: "",
+        password: "",
+    });
+
+    // 입력값이 변경될 때 실행되는 함수
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        // 기존의 form 상태를 복사한 후 해당 필드만 업데이트
+        setForm({
+            ...form,
+            [name]: value,
+        });
+    };
+
+    // 회원가입 버튼 클릭 시 실행되는 함수
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        try {
+            // axios를 사용하여 서버로 데이터를 보냅니다.
+            // const response = await signUp(form);
+            // console.log(response.message);
+        } catch (error) {
+            console.error("회원가입 실패:", error);
+        }
+    };
 
     return (
         <div className="login_container">
@@ -39,7 +68,50 @@ function SignIn() {
                 </div>
 
                 {/* 아이디 로그인 */}
-                <div></div>
+                <div className="login_id">
+                    <div className="login_id_title">아이디 로그인</div>
+                    <form onSubmit={handleSignUp}>
+                        <div className="id_container">
+                            <input
+                                id="id"
+                                type="text"
+                                name="id"
+                                value={form.id}
+                                onChange={handleChange}
+                                placeholder="아이디 또는 이메일"
+                                className="id"
+                            />
+                        </div>
+                        <div className="password_container">
+                            <input
+                                id="password"
+                                type="password"
+                                name="password"
+                                value={form.password}
+                                onChange={handleChange}
+                                placeholder="비밀번호"
+                                className="password"
+                            />
+                        </div>
+                        <div className="security_container">
+                            <div className="security_left">
+                                <div className="security_id">
+                                    <input type="checkbox" />
+                                    <div>아이디 저장</div>
+                                </div>
+                                <div className="security_sec">
+                                    <input type="checkbox" checked="true" />
+                                    <div>보안 접속</div>
+                                </div>
+                            </div>
+                            <div className="security_right">
+                                <div>아이디 찾기</div>
+                                <div>비밀번호 찾기</div>
+                            </div>
+                        </div>
+                        <button type="submit">로그인</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
